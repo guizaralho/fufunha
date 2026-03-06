d22ba5740f33b7fbc70bd98a866c04f07fe237ee
guizralho <guilhermesouzaitabuna@gmail.com>
2026-03-06 11:04:00 -0400

    OI

---
+++ backend/src/todos/repository/create-todo.repository.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from "src/shared/databases/prisma.database";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class CreateTodoRepository{
+    constructor(private readonly prisma: PrismaService) {}
+    async execute(data: CreateTodoDto){
+        return await this.prisma.todo.create({ data })
+    }
+}
